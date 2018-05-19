c70c73001a1dc46813a7d8a59636f6c3f80a60cc
TarunKS17 <tarunkumarsingh98@gmail.com>
2018-05-19 11:51:04 +0530

    Time stamp

--- compc.py
+++ compc.py
@@ -1,14 +1,16 @@
 #utf-8
-import subprocess, shlex
-
+import subprocess, shlex, time
+ 
 class compy:
     def __init__(self):
         self.run=[]
         self.filename=''
+        self.outputfile=str(time.time())+'.out'
     
     def compile(self, filename):
         self.filename=filename
-        a=subprocess.Popen(['g++', filename+'.cpp'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        args=shlex.split('g++ '+ filename + '.cpp -o ' + self.outputfile)
+        a=subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stats=a.communicate()
 
         if stats[0].decode() != '':
@@ -27,7 +29,8 @@ class compy:
         print("Testing:\n")
 
         while i<len(tc):
-            args=shlex.split('./a.out > '+self.filename+'.txt') #not working
+            #args=shlex.split(self.outputfile + ' > ' + self.filename+'.txt') #not working as it should
+            args=shlex.split('./'+self.outputfile)
             b=subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             b.stdin.write(tc[i].encode('utf-8'))
             b.stdin.close()
